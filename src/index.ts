727f19c96263094bed8430353989d64e5407ee88
Jacob Hands <jacob@gogit.io>
2023-04-01 22:13:11 -0500

    Add Queues

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,22 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
+interface QueueData {
+	pathname: string
+}
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
+	QUEUE: Queue<QueueData>
 }
 
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
+	async fetch(request: Request, env: Env, ctx: ExecutionContext
 	): Promise<Response> {
-		return new Response("Hello World!");
+		const url = new URL(request.url)
+		await env.QUEUE.send({ pathname: url.pathname })
+		return new Response("Hello World!")
 	},
+
+	async queue(batch: MessageBatch<QueueData>, env: Env, ctx: ExecutionContext) {
+		for (const message of batch.messages) {
+			console.log(message.body.pathname)
+			message.ack()
+		}
+	}
 };
